9ba11bade1b0144db63b25bf4c6b36b02e3e6f4f
Vlisie <40183592+Vlisie@users.noreply.github.com>
2022-12-05 15:46:51 +0100

    AoC dag 5

---
+++ src/Day05.kt
@@ -0,0 +1,79 @@
+import java.io.File
+
+fun main() {
+
+    fun part1(file: File): String {
+        val stapelLijst: List<ArrayDeque<Char>> = List(9) { ArrayDeque() }
+        file.readText()
+            .split("\n".toRegex())
+            .map { elem ->
+                var lastOccur = 0
+                for (char in elem) {
+                    if (char == '[') {
+                        lastOccur = elem.indexOf("[", lastOccur)
+                        stapelLijst[lastOccur / 4].addFirst(elem[lastOccur + 1])
+                        lastOccur++
+                    }
+                }
+                if (elem.startsWith("move")) {
+                    val cijfers = Regex("[0-9]+").findAll(elem)
+                        .map(MatchResult::value)
+                        .toList()
+                    repeat(cijfers[0].toInt()) {
+                        stapelLijst[(cijfers[2].toInt() - 1)].addLast(stapelLijst[(cijfers[1].toInt() - 1)].removeLast())
+                    }
+                }
+            }
+        var eindString = ""
+        for (stack in stapelLijst.asIterable()) {
+            eindString += if (stack.isNotEmpty()) stack.last() else ""
+        }
+        return eindString
+    }
+
+    fun part2(file: File): String {
+        val stapelLijst: List<ArrayDeque<Char>> = List(9) { ArrayDeque() }
+        file.readText()
+            .split("\n".toRegex())
+            .map { elem ->
+                var lastOccur = 0
+                for (char in elem) {
+                    if (char == '[') {
+                        lastOccur = elem.indexOf("[", lastOccur)
+                        stapelLijst[lastOccur / 4].addFirst(elem[lastOccur + 1])
+                        lastOccur++
+                    }
+                }
+                if (elem.startsWith("move")) {
+                    val cijfers = Regex("[0-9]+").findAll(elem)
+                        .map(MatchResult::value)
+                        .toList()
+                    val kraanStapel = ArrayDeque<Char>()
+                    repeat(cijfers[0].toInt()) {
+                        kraanStapel.addFirst(stapelLijst[(cijfers[1].toInt() - 1)].removeLast())
+                    }
+                    repeat(kraanStapel.size) {
+                        stapelLijst[(cijfers[2].toInt() - 1)].addLast(kraanStapel.removeFirst())
+                    }
+                }
+            }
+        var eindString = ""
+        for (stack in stapelLijst.asIterable()) {
+            eindString += if (stack.isNotEmpty()) stack.last() else ""
+        }
+        return eindString
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = File("src/input", "testInput.txt")
+    check(part1(testInput) == "CMZ")
+
+    check(part2(testInput) == "MCD")
+
+    val input = File("src/input", "input5.txt")
+    println("Part 1 -----------")
+    println(part1(input))
+    println("Part 2 -----------")
+    println(part2(input))
+
+}
